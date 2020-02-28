b33a9273a42e510dd54b9b5242da2fe4edd6b01b
Kong Wai Weng <waiweng83@gmail.com>
2020-02-28 11:00:58 +0800

    Renamed IR Sensor Bit to IR Bit.

--- ir_bit.ts
+++ ir_bit.ts
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Functions for edu:bit - IR Sensor Bit.
+ * Functions for edu:bit - IR Bit.
  *
  * Company: Cytron Technologies Sdn Bhd
  * Website: http://www.cytron.io
  * Email:   support@cytron.io
  *******************************************************************************/
 
-// Possible pins for IR sensor.
-enum IrSensorPin {
+// Possible pins for IR Bit.
+enum IrBitPin {
     //% block="P8 (Default)"
     P8 = DigitalPin.P8,
     P0 = DigitalPin.P0,
@@ -20,7 +20,7 @@ enum IrSensorPin {
     P16 = DigitalPin.P16,
 }
 
-// IR Event Type.
+// IR sensor Event Type.
 enum IrEventType {
     //% block="Not Triggered"
     Fall = EventBusValue.MICROBIT_PIN_EVT_FALL,
@@ -32,33 +32,33 @@ enum IrEventType {
 
 
 /**
- * Blocks for IR Sensor Bit.
+ * Blocks for IR Bit.
  */
-//% weight=14 color=#ff8000 icon="\uf05b" block="IR Sensor"
-namespace edubit_ir_sensor {
+//% weight=14 color=#ff8000 icon="\uf05b" block="IR Bit"
+namespace edubit_ir_bit {
 
     /**
      * Return IR sensor state (0 or 1).
-     * @param pin Pin number for IR sensor. eg: IrSensorPin.P8
+     * @param pin Pin number for IR Bit. eg: IrBitPin.P8
      */
     //% blockGap=8
-    //% blockId=edubit_read_ir_sensor
+    //% blockId=edubit_irbit_read_ir_sensor
     //% block="Read IR sensor || at pin %pin"
     //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=3
-    export function readIrSensor(pin: IrSensorPin = IrSensorPin.P8): number {
+    export function readIrSensor(pin: IrBitPin = IrBitPin.P8): number {
         return pins.digitalReadPin(<number>pin);
     }
 
 
     /**
      * Return true if IR sensor is triggered.
-     * @param pin Pin number for IR sensor. eg: IrSensorPin.P8
+     * @param pin Pin number for IR Bit. eg: IrBitPin.P8
      */
     //% blockGap=30
-    //% blockId=edubit_is_ir_sensor_triggered
+    //% blockId=edubit_irbit_is_ir_sensor_triggered
     //% block="IR sensor's triggered || at pin %pin"
     //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=3
-    export function isIrSensorTriggered(pin: IrSensorPin = IrSensorPin.P8): boolean {
+    export function isIrSensorTriggered(pin: IrBitPin = IrBitPin.P8): boolean {
         if (pins.digitalReadPin(<number>pin) != 0) {
             return true;
         }
@@ -71,13 +71,13 @@ namespace edubit_ir_sensor {
     /**
     * Do something when IR sensor is triggered / not triggered.
     * @param event Event type. eg: IrEventType.Rise
-    * @param pin Pin number for IR sensor. eg: IrSensorPin.P8
+    * @param pin Pin number for IR Bit. eg: IrBitPin.P8
     */
     //% blockGap=8
-    //% blockId=edubit_ir_sensor_event
+    //% blockId=edubit_irbit_ir_sensor_event
     //% block="On IR sensor's %event at pin %pin"
     //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=3
-    export function onIrSensorEvent(event: IrEventType, pin: IrSensorPin, handler: Action) {
+    export function onIrSensorEvent(event: IrEventType, pin: IrBitPin, handler: Action) {
         // Set the event type as edge triggered.
         pins.setEvents(<number>pin, PinEventType.Edge);
 
@@ -90,18 +90,18 @@ namespace edubit_ir_sensor {
     /**
     * Get the event source based on pin number.
     */
-    function getEventSource(pin: IrSensorPin): EventBusSource {
+    function getEventSource(pin: IrBitPin): EventBusSource {
         // Get the event source based on pin number.
         switch (pin) {
-            case IrSensorPin.P0: return EventBusSource.MICROBIT_ID_IO_P0;
-            case IrSensorPin.P1: return EventBusSource.MICROBIT_ID_IO_P1;
-            case IrSensorPin.P2: return EventBusSource.MICROBIT_ID_IO_P2;
-            case IrSensorPin.P8: return EventBusSource.MICROBIT_ID_IO_P8;
-            case IrSensorPin.P12: return EventBusSource.MICROBIT_ID_IO_P12;
-            case IrSensorPin.P13: return EventBusSource.MICROBIT_ID_IO_P13;
-            case IrSensorPin.P14: return EventBusSource.MICROBIT_ID_IO_P14;
-            case IrSensorPin.P15: return EventBusSource.MICROBIT_ID_IO_P15;
-            case IrSensorPin.P16: return EventBusSource.MICROBIT_ID_IO_P16;
+            case IrBitPin.P0: return EventBusSource.MICROBIT_ID_IO_P0;
+            case IrBitPin.P1: return EventBusSource.MICROBIT_ID_IO_P1;
+            case IrBitPin.P2: return EventBusSource.MICROBIT_ID_IO_P2;
+            case IrBitPin.P8: return EventBusSource.MICROBIT_ID_IO_P8;
+            case IrBitPin.P12: return EventBusSource.MICROBIT_ID_IO_P12;
+            case IrBitPin.P13: return EventBusSource.MICROBIT_ID_IO_P13;
+            case IrBitPin.P14: return EventBusSource.MICROBIT_ID_IO_P14;
+            case IrBitPin.P15: return EventBusSource.MICROBIT_ID_IO_P15;
+            case IrBitPin.P16: return EventBusSource.MICROBIT_ID_IO_P16;
         }
         return null;
     }
