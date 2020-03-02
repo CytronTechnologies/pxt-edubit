e78212961107af366a1abd9987e6824c845a48e3
Kong Wai Weng <waiweng@cytron.com.my>
2020-03-02 11:41:47 +0800

    Simplified the extension by removing the ability to change the pin.

--- rgb_bit.ts
+++ rgb_bit.ts
@@ -6,19 +6,8 @@
  * Email:   support@cytron.io
  *******************************************************************************/
 
-// Possible pins for RGB Bit.
-enum RgbBitPin {
-    //% block="P13*"
-    P13 = DigitalPin.P13,
-    P0 = DigitalPin.P0,
-    P1 = DigitalPin.P1,
-    P2 = DigitalPin.P2,
-    P8 = DigitalPin.P8,
-    P12 = DigitalPin.P12,
-    P14 = DigitalPin.P14,
-    P15 = DigitalPin.P15,
-    P16 = DigitalPin.P16,
-};
+// Default pin.
+const RGB_BIT_PIN = DigitalPin.P13;
 
 // Number of LEDs in RGB Bit.
 const RGB_BIT_LENGTH = 4;
@@ -37,7 +26,7 @@ namespace edubit_rgb_bit {
     }
 
     // Create a Neo Pixel object for RGB Bit.
-    let rgbBit = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB);
+    let rgbBit = neopixel.create(RGB_BIT_PIN, 4, NeoPixelMode.RGB);
     rgbBit.clear();
 
     // Reduce the default brightness.
@@ -46,37 +35,53 @@ namespace edubit_rgb_bit {
 
 
     /**
-     * Turn off all pixels. 
-     * @param pin Pin number for RGB Bit.
+     * Turn off all RGB pixels.
      */
-    //% blockGap=30
-    //% blockId="edubit_rgbbit_clear"
-    //% block="clear all pixels on %pin"
-    export function clear(pin: RgbBitPin): void {
+    //% blockGap=8
+    //% blockId="edubit_clear_pixels"
+    //% block="clear all RGB pixels"
+    export function clear(): void {
         for (let i = 0; i < RGB_BIT_LENGTH; i++) {
             colorsArray[i] = 0;
         }
 
-        rgbBit.setPin(<number>pin);
         rgbBit.clear();
         basic.pause(0);
     }
 
 
     /**
-     * Show a rainbow pattern on all pixels.
-     * @param pin Pin number for RGB Bit.
+     * Set the brightness of the RGB pixels (0-255).
+     * @param brightness Pixel brightness. eg: 25
+     */
+    //% blockGap=40
+    //% blockId="edubit_set_brightness"
+    //% block="set RGB pixels brightness to %brightness"
+    //% brightness.min=0 brightness.max=255
+    export function setBrightness(brightness: number): void {
+        rgbBit.setBrightness(brightness);
+
+        // Restore the original color.
+        for (let i = 0; i < RGB_BIT_LENGTH; i++) {
+            rgbBit.setPixelColor(i, colorsArray[i]);
+        }
+        rgbBit.show();
+        basic.pause(0);
+    }
+
+
+    /**
+     * Show a rainbow pattern on all RGB pixels.
      */
     //% blockGap=8
-    //% blockId="edubit_rgbbit_show_rainbow"
-    //% block="show rainbow colors on %pin"
-    export function showRainbow(pin: RgbBitPin): void {
+    //% blockId="edubit_show_rainbow"
+    //% block="show rainbow on RGB pixels"
+    export function showRainbow(): void {
         colorsArray[0] = NeoPixelColors.Red;
         colorsArray[1] = NeoPixelColors.Yellow;
         colorsArray[2] = NeoPixelColors.Green;
         colorsArray[3] = NeoPixelColors.Indigo;
 
-        rgbBit.setPin(<number>pin);
         for (let i = 0; i < RGB_BIT_LENGTH; i++) {
             rgbBit.setPixelColor(i, colorsArray[i]);
         }
@@ -87,15 +92,13 @@ namespace edubit_rgb_bit {
 
 
     /**
-     * Show the same color on all pixels. 
+     * Show the same color on all RGB pixels. 
      * @param color RGB color of the pixel.
-     * @param pin Pin number for RGB Bit.
      */
     //% blockGap=8
-    //% blockId="edubit_rgbbit_show_color"
-    //% block="set all pixels on %pin to %color"
-    export function showColor(pin: RgbBitPin, color: NeoPixelColors): void {
-        rgbBit.setPin(<number>pin);
+    //% blockId="edubit_show_color"
+    //% block="set all RGB pixels to %color"
+    export function showColor(color: NeoPixelColors): void {
         for (let i = 0; i < RGB_BIT_LENGTH; i++) {
             colorsArray[i] = <number>color;
         }
@@ -105,17 +108,15 @@ namespace edubit_rgb_bit {
 
 
     /**
-     * Show color on individual pixel.
+     * Show color on individual RGB pixel.
      * @param pixel The pixel number we want to change the color.
      * @param color RGB color of the pixel.
-     * @param pin Pin number for RGB Bit.
      */
-    //% blockGap=30
-    //% blockId="edubit_rgbbit_set_pixel_color"
-    //% block="set pixel %pixel on %pin to %color"
+    //% blockGap=40
+    //% blockId="edubit_set_pixel_color"
+    //% block="set RGB pixel %pixel to %color"
     //% pixel.min=0 pixel.max=3
-    export function setPixelColor(pixel: number, pin: RgbBitPin, color: NeoPixelColors): void {
-        rgbBit.setPin(<number>pin);
+    export function setPixelColor(pixel: number, color: NeoPixelColors): void {
         colorsArray[pixel] = <number>color;
         rgbBit.setPixelColor(pixel, <number>color);
         rgbBit.show();
@@ -124,15 +125,14 @@ namespace edubit_rgb_bit {
 
 
     /**
-     * Shift the pixels on RGB Bit (-3 to 3).
+     * Shift the color of RGB pixels (-3 to 3).
      * @param offset Number of pixels to shift.
-     * @param pin Pin number for RGB Bit.
      */
     //% blockGap=8
-    //% blockId="edubit_rgbbit_shift_pixels"
-    //% block="shift pixels on %pin by %offset"
+    //% blockId="edubit_shift_pixels"
+    //% block="shift RGB pixels color by %offset"
     //% offset.min=-3 offset.max=3
-    export function shiftPixels(pin: RgbBitPin, offset: number): void {
+    export function shiftPixels(offset: number): void {
         // Do nothing if offset is 0.
         if (offset == 0) {
             return;
@@ -160,8 +160,6 @@ namespace edubit_rgb_bit {
         }
 
 
-        rgbBit.setPin(<number>pin);
-
         // Show the new color.
         for (let i = 0; i < RGB_BIT_LENGTH; i++) {
             rgbBit.setPixelColor(i, colorsArray[i]);
@@ -172,15 +170,14 @@ namespace edubit_rgb_bit {
 
 
     /**
-     * Rotate the pixels on RGB Bit (-3 to 3).
+     * Rotate the color of RGB pixels(-3 to 3).
      * @param offset Number of pixels to rotate.
-     * @param pin Pin number for RGB Bit.
      */
     //% blockGap=30
-    //% blockId="edubit_rgbbit_rotate_pixels"
-    //% block="rotate pixels on %pin by %offset"
+    //% blockId="edubit_rotate_pixels"
+    //% block="rotate RGB pixels color by %offset"
     //% offset.min=-3 offset.max=3
-    export function rotatePixels(pin: RgbBitPin, offset: number): void {
+    export function rotatePixels(offset: number): void {
         // Do nothing if offset is 0.
         if (offset == 0) {
             return;
@@ -210,8 +207,6 @@ namespace edubit_rgb_bit {
         }
 
 
-        rgbBit.setPin(<number>pin);
-
         // Show the new color.
         for (let i = 0; i < RGB_BIT_LENGTH; i++) {
             rgbBit.setPixelColor(i, colorsArray[i]);
@@ -220,26 +215,5 @@ namespace edubit_rgb_bit {
         basic.pause(0);
     }
 
-
-    /**
-     * Set the brightness of the RGB Bit (0-255).
-     * @param brightness pixel brightness. eg: 25
-     * @param pin Pin number for RGB Bit.
-     */
-    //% blockGap=8
-    //% blockId="edubit_rgbbit_set_brightness"
-    //% block="set brightness on %pin to %brightness"
-    //% brightness.min=0 brightness.max=255
-    export function setBrightness(pin: RgbBitPin, brightness: number): void {
-        rgbBit.setPin(<number>pin);
-        rgbBit.setBrightness(brightness);
-
-        // Restore the original color.
-        for (let i = 0; i < RGB_BIT_LENGTH; i++) {
-            rgbBit.setPixelColor(i, colorsArray[i]);
-        }
-        rgbBit.show();
-        basic.pause(0);
-    }
 }
 
